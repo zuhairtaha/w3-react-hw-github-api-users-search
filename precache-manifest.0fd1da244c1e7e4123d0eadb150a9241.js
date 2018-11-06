8bc55e0d62cfb9067a591e7e2d46392d08388097
Zuhair Taha <admin@tahasoft.com>
2018-11-07 00:06:14 +0100

    Updates

---
+++ precache-manifest.0fd1da244c1e7e4123d0eadb150a9241.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee877fb06c1e18bf2829",
+    "url": "/w3-react-hw-github-api-users-search/static/js/runtime~main.ee877fb0.js"
+  },
+  {
+    "revision": "80f8f58659cf444e447e",
+    "url": "/w3-react-hw-github-api-users-search/static/js/main.80f8f586.chunk.js"
+  },
+  {
+    "revision": "278b04fab6d7fcc38e5a",
+    "url": "/w3-react-hw-github-api-users-search/static/js/1.278b04fa.chunk.js"
+  },
+  {
+    "revision": "80f8f58659cf444e447e",
+    "url": "/w3-react-hw-github-api-users-search/static/css/main.e849ff06.chunk.css"
+  },
+  {
+    "revision": "51664e4e19b27c9f9e750bd334bcb6d6",
+    "url": "/w3-react-hw-github-api-users-search/index.html"
+  }
+];
